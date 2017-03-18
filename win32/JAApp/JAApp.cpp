65770e92879bf99c5888be373f144beb8cc3379a
ja <ja@DESKTOP-NGP6PGE>
2017-03-18 02:27:21 +0100

    lab1

--- win32/JAApp/JAApp.cpp
+++ win32/JAApp/JAApp.cpp
@@ -5,12 +5,17 @@
 #include <iostream>
 #include<Windows.h>
 using namespace std;
+typedef int(_stdcall *MYPROC)(DWORD x, DWORD y);
 extern "C" int _stdcall MyProc1(DWORD x, DWORD y);
 int _tmain(int argc, _TCHAR* argv[])
 {
+	HMODULE dll = LoadLibrary(L"../Debug/JADll");
+	MYPROC f = (MYPROC) GetProcAddress(dll,"MyProc1");
+	f(1, 1);
 	int x=1,y=2;
 	int z;
 	z = MyProc1(x,y);
-	cout<<z<<endl;
+	cout << z << endl;
+	FreeLibrary(dll);
 	return 0;
 }
--- win32/JADll/mojadll.asm
+++ win32/JADll/mojadll.asm
@@ -18,7 +18,19 @@ DllEntry ENDP
 MyProc1 proc x:DWORD, y:DWORD
 	
 	xor eax,eax;
-	mov eax,x
+	add eax,128;
+	add eax,1
+	STD;
+	POPF
+	CLD
+	mov eax,4294967295;2^32-1
+	add eax,1
+	mov eax,2147483647
+	add eax,1
+	xor eax,eax;
+	mov eax,8
+	add eax,8
+	xor eax,eax;
 	mov ecx, y
 	ror ecx, 1
 	shld eax,ecx,2
